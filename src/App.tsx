bfda0d35c94e9fcdd25e0fea85646c04d8872c4b
kharle14 <enriquezkharl14@gmail.com>
2024-02-04 20:53:34 +0800

    [Feat] Added supporters content

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from './features/header/Header';
 import HeroContent from './features/hero/HeroContent';
 import HowItWorksContent from './features/how-it-works/HowItWorksContent';
 import PlansContent from './features/plans/PlansContent';
+import SupportedContent from './features/supported/SupportedContent';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <AboutContent />
       <HowItWorksContent />
       <PlansContent />
+      <SupportedContent />
     </main>
   );
 }
---
+++ src/features/supported/SupportedContent.tsx
@@ -0,0 +1,46 @@
+import { GiAbstract002, GiAbstract037, GiAbstract086 } from 'react-icons/gi';
+import styles from './supportContent.module.css';
+
+export default function SupportedContent() {
+  return (
+    <div className={styles.support}>
+      <div className={styles.gratitude}>
+        <h2>TranquilVibes Gratitude</h2>
+        <p>
+          We extend our heartfelt thanks to the following companies that contribute to the serenity
+          and well-being of our community
+        </p>
+      </div>
+      <div className={styles.supporters}>
+        <div className={styles.supporter}>
+          <h4>
+            <GiAbstract002 /> <span>MindfulLife Co.</span>
+          </h4>
+          <p>
+            A leader in mindful living, MindfulLife Co. shares our commitment to fostering
+            tranquility in everyday life.
+          </p>
+        </div>
+        <div className={styles.supporter}>
+          <h4>
+            <GiAbstract037 /> <span>SerenityTech Solutions</span>
+          </h4>
+          <p>
+            SerenityTech Solutions provides cutting-edge technology that enhances our virtual
+            wellness retreat experiences.
+          </p>
+        </div>
+        <div className={styles.supporter}>
+          <h4>
+            <GiAbstract086 />
+            <span>ZenHarbor Retreats</span>
+          </h4>
+          <p>
+            ZenHarbor Retreats, a premier wellness retreat organizer, collaborates with us to bring
+            you transformative experiences.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
